6fcc720e89b9165570eaa12ae93ccf0a5012d8bf
arthur souza <arthur.ghz@icloud.com>
2023-04-29 20:56:20 -0300

    fix: cors

--- main.go
+++ main.go
@@ -1,46 +1,51 @@
 package main
 
 import (
-	"context"
-
-	"mood-api/handlers"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-	"github.com/gin-gonic/gin"
+    "context"
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+    "mood-api/handlers"
 )
 
 var ginLambda *ginadapter.GinLambda
 
 const (
-	healthPath = "/api/health"
-	moodPathV1   = "/api/v1/mood"
+    healthPath = "/api/health"
+    moodPathV1 = "/api/v1/mood"
 )
 
 func init() {
-	g := gin.Default()
+    g := gin.Default()
 
-	// Define health route
-	g.GET(healthPath, func(c *gin.Context) {
-		handlers.PingHandler(c.Writer, c.Request)
-	})
+    // Define health route
+    g.GET(healthPath, func(c *gin.Context) {
+        handlers.PingHandler(c.Writer, c.Request)
+    })
 
-	g.POST(moodPathV1, func(c *gin.Context) {
-		handlers.MoodHandler(c.Writer, c.Request)
-	})
-	g.GET(moodPathV1, func(c *gin.Context) {
-		handlers.MoodHandler(c.Writer, c.Request)
-	})
+    g.POST(moodPathV1, func(c *gin.Context) {
+        handlers.MoodHandler(c.Writer, c.Request)
+    })
+    g.GET(moodPathV1, func(c *gin.Context) {
+        handlers.MoodHandler(c.Writer, c.Request)
+    })
 
-	// Create Gin Lambda instance
-	ginLambda = ginadapter.New(g)
+    // Configure CORS
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"https://www.worldmoodtoday.com"}
+    config.AllowMethods = []string{"GET", "POST", "OPTIONS"}
+    g.Use(cors.New(config))
+
+    // Create Gin Lambda instance
+    ginLambda = ginadapter.New(g)
 }
 
 func main() {
-	lambda.Start(Handler)
+    lambda.Start(Handler)
 }
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return ginLambda.ProxyWithContext(ctx, request)
+    return ginLambda.ProxyWithContext(ctx, request)
 }
